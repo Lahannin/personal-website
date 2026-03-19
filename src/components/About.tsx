775b91305e8baa387c15279dd564684b19777881
Lauri Hänninen <36676006+Lahannin@users.noreply.github.com>
2026-03-19 13:53:42 +0100

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -19,7 +19,7 @@ const About = () => {
     },
     {
       title: "People leader",
-      description: "Experienced in managing tech teams and leading 25+ conscripts in the Finnish army."
+      description: "Experienced in leading tech teams and 25+ conscripts in the Finnish army."
     },
     {
       title: "Biz + tech mindset",

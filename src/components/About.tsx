19722e3aa94127f6985f1fef2127e4090ad76416
Lauri Hänninen <36676006+Lahannin@users.noreply.github.com>
2026-03-20 16:04:20 +0100

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -19,7 +19,7 @@ const About = () => {
     {
       icon: Zap,
       title: "Community Builder",
-      description: "Founded and leads the first Product Marketing Alliance chapter in Prague.",
+      description: "Founded and leads the first Product Marketing Alliance chapter in Prague, Czechia.",
       },
   ];
 
